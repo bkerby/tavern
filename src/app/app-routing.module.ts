d7a187d0818891a989257afec42063b85847f07d
Benjamin Kerby <bkerby@unomaha.edu>
2019-06-06 19:39:59 -0500

    Refactored pages for organizational purposes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthService } from '../services/auth/auth.service';
+import { AuthService } from './services/auth/auth.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthService] }
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule', canActivate: [AuthService] }
 ];
 
 @NgModule({
